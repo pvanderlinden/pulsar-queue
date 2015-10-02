f1b322e398851a6ec3f5fff695f371ac70ad51f8
Yaroslav Bikov <yaroslavbikov@bmlltech.com>
2015-10-02 17:50:48 +0100

    Pool.connect hacked for the test

--- tests/task_app.py
+++ tests/task_app.py
@@ -6,7 +6,8 @@ from unittest.mock import MagicMock, Mock
 from pulsar.async.clients import Pool
 
 from pq import TaskApp
-
+from pulsar.apps.data import create_store
+from pq.pubsub import PubSub
 
 class TestTaskApp(unittest.TestCase):
 
@@ -15,38 +16,33 @@ class TestTaskApp(unittest.TestCase):
         task_app = TaskApp(name='mytest', task_paths=['tests.example.sampletasks.*'])
         task_app.start()
         cls.task_app = task_app
+        cls.pool_connect = {True: Pool.connect, False: Mock(side_effect=ConnectionRefusedError('test'))} 
+
+    def set_connection(self, enabled=True):
+        backend = self.task_app.backend
+        client = backend._pubsub._client
+        pubsub = backend._pubsub
+        client = backend._pubsub._client
+        #close
+        client.store.pool.close(in_use=True)
+        # hack
+        Pool.connect = self.pool_connect[enabled]
+        # create
+        data_store = pubsub.cfg.data_store
+        client.store = create_store(data_store)
+        backend._pubsub = PubSub(backend)
 
     def test_test(self):
         backend = self.task_app.backend
         pubsub = backend._pubsub
+        
         task_fut = backend.queue_task('addition', a=1, b=2, wait=False)
-#         print (pubsub._pubsub._connection)
-#         pubsub._pubsub._connection.open()
-#         pubsub._pubsub.open()
-        client = backend._pubsub._client
-        # close
-        client.store.pool.close(in_use=True)
         
-        # must fail
-        try:
-            yield from pubsub.flush_queues()
-        except:
-            print ("failed")
-        
-        
-        # hack
-        Pool.connect = Mock(side_effect=ConnectionRefusedError('test'))
-        
-        
-        # open
-        from pulsar.apps.data import create_store
-        from pq.pubsub import PubSub
-        data_store = pubsub.cfg.data_store
-        client.store = create_store(data_store)
-        backend._pubsub = PubSub(backend)
+        self.set_connection(True)
         yield from pubsub.flush_queues()
         
-#         connection = Mock(side_effect=ConnectionRefusedError('test'))
-#         connection()
-#         pass
+        self.set_connection(False)
+        yield from pubsub.flush_queues()
+        
+
         
